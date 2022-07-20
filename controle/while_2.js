03e8d4a2340eebf7b7c47aabf8a16c0bb7ff657f
SenaJulio <gm.sena@hotmail.com>
2022-07-20 09:49:46 -0300

    novas aulas

---
+++ controle/while_2.js
@@ -0,0 +1,11 @@
+const futuro = Date.now() + 10000;
+let quantidade = 0;
+
+
+while(Date.now() < futuro){
+    quantidade++;
+}
+
+//Qtde: 32543784
+//Qtde:32525345
+console.log('Qtde:' + quantidade);
